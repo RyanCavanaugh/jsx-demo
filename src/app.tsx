f6844ac054d86a3d5d09c6941a7eeddab645b17b
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2015-07-30 15:30:46 -0700

    Friendly greetings

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,27 @@
 import React = require('react');
 import $ = require('jquery');
 
+interface GreeterProps extends React.Props<Greeter> {
+	whoToGreet: string;
+	greeting?: string;
+}
+class Greeter extends React.Component<GreeterProps, {}> {
+	render() {
+		let greeting = this.props.greeting || 'Hello';
+		return <div>{greeting}, {this.props.whoToGreet}</div>;
+	}
+}
 
+function getRandomGreeting() {
+	switch(Math.floor(Math.random() * 4)) {
+		case 0: return 'Hello';
+		case 1: return 'Howdy';
+		case 2: return 'Greeting';
+		case 3: return 'Hail';
+	}
+}
 
 $(() => {
-	React.render(<div>Hello, world</div>, $('#output').get(0));
+	React.render(<Greeter whoToGreet='world' greeting='Hello' />, $('#output').get(0));
 });
 
