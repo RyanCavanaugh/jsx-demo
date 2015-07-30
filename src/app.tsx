3c21118ba0c2af8d32c070546698c3c35d6a1a3b
Ryan Cavanaugh <RyanCavanaugh@users.noreply.github.com>
2015-07-30 15:37:02 -0700

    Union types and type guards

--- src/app.tsx
+++ src/app.tsx
@@ -6,12 +6,12 @@ function getRandomGreeting() {
 	switch(Math.floor(Math.random() * 4)) {
 		case 0: return 'Hello';
 		case 1: return 'Howdy';
-		case 2: return 'Greeting';
+		case 2: return 'Greetings to you';
 		case 3: return 'Hail';
 	}
 }
 
 $(() => {
-	React.render(<Greetifier whoToGreet='world' greeting='Hello' />, $('#output').get(0));
+	React.render(<Greetifier whoToGreet='world' greeting={getRandomGreeting} />, $('#output').get(0));
 });
 
--- src/greeter.tsx
+++ src/greeter.tsx
@@ -2,12 +2,20 @@ import React = require('react');
 
 interface GreeterProps extends React.Props<Greeter> {
 	whoToGreet: string;
-	greeting?: string;
+	greeting?: string|(() => string);
 }
 
 export class Greeter extends React.Component<GreeterProps, {}> {
 	render() {
-		let greeting = this.props.greeting || 'Hello';
+		let g = this.props.greeting;
+
+		let greeting = 'Hello';
+		if(typeof g === 'string') {
+			greeting = g;
+		} else if(g) {
+			greeting = g();
+		}
+
 		return <div>{greeting}, {this.props.whoToGreet}</div>;
 	}
 }
